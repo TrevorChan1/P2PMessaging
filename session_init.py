a1aeacd9fc12ed84758722e675aafeb3edd1b31a
Trevor Chan <trevchan@bu.edu>
2023-03-28 19:09:12 -0400

    Should be working possible hopefully

--- session_init.py
+++ session_init.py
@@ -20,9 +20,10 @@ def sendMessage(host, port, message):
     print(reply)
 
 # Class for people currently sending messages
-class sender(threading.Thread):
+# def sender():
 
-    def __init__(self, host, port):
+
+
 
 # Function that will constantly wait for messages then print them
 def receiveMessage(host, port):
@@ -31,7 +32,26 @@ def receiveMessage(host, port):
     r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     r.bind(host,port)
 
-    # 
+    # Accept connections
+    ip, addr = r.accept()
+
+    # When it gets a connection, check if the ip and address are a known user
+    user = [n for n, values in users.items() if values == [ip,addr]]
+    if (user):
+        name = user
+    else:
+        name = "UNKNOWN"
+
+    # While receiving messages, print out the messages
+    while True:
+        message = r.recv(1024)
+        print(name + ":", message)
+    
+    # Close the connection if possible
+    r.close()
+
+
+    
 
 # Function that opens user to be messaged and sent message to
 def p2pMessager(name, port):
@@ -46,5 +66,38 @@ def p2pMessager(name, port):
 
     # Create a thread for the receiver with the host and port as arguments
     threading.Thread(receiveMessage, (host, port))
+
+    messager_open = True
+    dest_socket = None
+
+    # Loop until told to close the messager
+    while (messager_open):
+
+        # If there is no destination, ask for a name
+        if (dest_socket == None):
+            dest = input("Enter Receiver Name: ")
+
+            # If told to exit, leave
+            if (dest == "exit"):
+                messager_open = False
+                break
+            
+            # Otherwise, check if destination is a known user
+            if (dest in users):
+                dest_socket = users[dest]
+            else:
+                print("User not found")
+                dest_socket = None
+
+        # If a user is known, then inquire for message to send
+        else:
+            message = input("Enter message: ")
+
+            # If asked to exit, exit the current user
+            if (message == "exit"):
+                dest_socket = None
+            # Otherwise, create a thread sending the message
+            else:
+                threading.Thread(sendMessage, (dest_socket[0], dest_socket[1], message))
 
     
